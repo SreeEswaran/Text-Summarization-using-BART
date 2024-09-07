dfca577ff04c66d7bd28458c1c89602676a7510d
Sree Deekshitha Yerra <126445402+SreeEswaran@users.noreply.github.com>
2024-09-07 22:57:04 +0530

    Update data_preprocess.py

--- scripts/data_preprocess.py
+++ scripts/data_preprocess.py
@@ -6,7 +6,6 @@ def preprocess_data(input_file, output_dir):
     df = pd.read_csv(input_file)
     train, val_test = train_test_split(df, test_size=0.2, random_state=42)
     val, test = train_test_split(val_test, test_size=0.5, random_state=42)
-    
     os.makedirs(output_dir, exist_ok=True)
     train.to_csv(os.path.join(output_dir, 'train.csv'), index=False)
     val.to_csv(os.path.join(output_dir, 'val.csv'), index=False)
